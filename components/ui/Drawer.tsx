b3bbae804f14ef6f4396d1777e1ae489c3e213c6
Bruno Rossetto <brunoccr@gmail.com>
2026-04-29 11:51:35 -0300

    - Alteração de estilizações

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -39,7 +39,7 @@ export const DrawerItem = ({
     return (
       <a
         href={route}
-        className="w-full border-b border-slate-700 pb-2 hover:text-indigo-500"
+        className="w-full border-b text-base border-b-gray-800 pb-2 hover:text-indigo-500"
       >
         <div className="flex gap-4 items-center">
           {icon}
@@ -105,7 +105,7 @@ export const Drawer = ({
   };
 
   return (
-    <nav className="bg-white text-black dark:bg-black dark:text-white p-2 pr-5 pl-5 border-b border-b-gray-600 fixed w-full z-50">
+    <nav className="bg-white text-black dark:bg-[#111318] dark:text-white p-2 pr-5 pl-5 border-b border-b-gray-800 fixed w-full z-50">
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-4 w-full">
           {/******** Menu Hamburguer *******/}
@@ -127,7 +127,7 @@ export const Drawer = ({
           </div>
           {/******** Título e Subtítulo *******/}
           <div className="flex-1 flex flex-col">
-            <span className="text-xl font-bold">{title}</span>
+            <span className="text-base font-bold">{title}</span>
             <span className="text-xs text-gray-400">{subtitle}</span>
           </div>
           {/******** Filtro *******/}
@@ -135,9 +135,9 @@ export const Drawer = ({
             <ButtonMenu
               content={
                 <div className="flex flex-col justify-center items-center w-8 h-8 space-y-1 focus:outline-none relative">
-                  <span className="block w-4 h-0.5 bg-white"></span>
-                  <span className="block w-2 h-0.5 bg-white"></span>
-                  <span className="block w-0.5 h-0.5 bg-white"></span>
+                  <span className="block w-5 h-0.5 bg-white"></span>
+                  <span className="block w-3 h-0.5 bg-white"></span>
+                  <span className="block w-1 h-0.5 bg-white"></span>
                 </div>
               }
             >
@@ -160,7 +160,7 @@ export const Drawer = ({
       )}
       {/********* Menu ********/}
       <div
-        className={`fixed top-0 left-0 h-full w-96 bg-slate-800 transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out z-40 shadow-2xl`}
+        className={`fixed top-0 left-0 h-full w-96 max-w-[90%] bg-[#111318] transform ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out z-40 shadow-2xl`}
       >
         <div className="flex flex-col items-start p-8 space-y-6 mt-16 text-lg font-medium">
           <div className="flex flex-col items-center w-full">
